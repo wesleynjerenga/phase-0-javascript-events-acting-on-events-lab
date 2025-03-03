18ae477f647add209ab2508d4b51071f4d0c0e00
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-03 15:45:42 +0300

    javascript- events

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+function moveDodgerLeft() {
+  const dodger = document.getElementById('dodger');
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+	dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger');
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+	dodger.style.left = `${left + 1}px`;
+  }
+}
